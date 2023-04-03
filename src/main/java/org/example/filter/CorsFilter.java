c683be83fb444203221692043709a15a89ef73ee
imangali <i.kaliakhmetov@strongte.am>
2023-04-03 23:17:05 +0600

    fix corsfilter0

---
+++ src/main/java/org/example/filter/CorsFilter.java
@@ -0,0 +1,26 @@
+package org.example.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@Component
+public class CorsFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        chain.doFilter(req, res);
+    }
+
+    public void destroy() {
+    }
+}
