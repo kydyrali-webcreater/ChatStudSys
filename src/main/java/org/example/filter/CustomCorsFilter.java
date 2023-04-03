490790e4d29df73cb661c51ec4e3a7d1d4bb66de
imangali <i.kaliakhmetov@strongte.am>
2023-04-03 23:03:21 +0600

    fix corsfilter

---
+++ src/main/java/org/example/filter/CustomCorsFilter.java
@@ -0,0 +1,35 @@
+package org.example.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@Component
+public class CustomCorsFilter extends OncePerRequestFilter {
+
+    @Value(value = "${cors.allowedOrigin}")
+    private String allowedOrigin;
+
+    @Value(value = "${cors.enabled}")
+    private Boolean corsEnabled;
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", getAllowedOrigin());
+        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, DELETE, PUT, PATCH, HEAD");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
+        response.setIntHeader("Access-Control-Max-Age", 10);
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAllowedOrigin() {
+        return corsEnabled ? allowedOrigin : "*";
+    }
+}
