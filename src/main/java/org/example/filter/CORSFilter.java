eee3e62b1dd801ed956c216e1b48b940727a5c92
imangali <i.kaliakhmetov@strongte.am>
2023-04-03 22:35:11 +0600

    add cors filter1

--- src/main/java/org/example/filter/CORSFilter.java
+++ src/main/java/org/example/filter/CORSFilter.java
@@ -13,10 +13,14 @@ public class CORSFilter implements Filter {
             throws ServletException, IOException {
 
         HttpServletResponse response = (HttpServletResponse) res;
+
+        // Set CORS headers
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+
+        // Pass request down the chain, except for OPTIONS
         if (((HttpServletRequest) req).getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
