a0f590f97b5bc000cd24b3993794a6fb5d0d06e6
Imaka <79092624+Imangali-ang@users.noreply.github.com>
2023-04-04 22:55:34 +0600

    init else cors test

--- src/main/java/org/example/AttendanceSystemApplication.java
+++ src/main/java/org/example/AttendanceSystemApplication.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import org.example.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
 @SpringBootApplication
 public class AttendanceSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(AttendanceSystemApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
     }
 }
