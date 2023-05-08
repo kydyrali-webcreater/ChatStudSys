fcdbbba8876d885e2c2eabdebc501fb3296d805a
Imaka <79092624+Imangali-ang@users.noreply.github.com>
2023-05-08 20:13:20 +0600

    exceptionHandler code

--- src/main/java/org/example/Exceptions/BasicExceptionHandler.java
+++ src/main/java/org/example/Exceptions/BasicExceptionHandler.java
@@ -4,6 +4,8 @@ import org.example.Exceptions.models.BasicException;
 import org.example.Exceptions.models.ResponseError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,10 +15,10 @@ public class BasicExceptionHandler {
     private final Logger LOG = LoggerFactory.getLogger(ExceptionHandler.class);
 
     @ExceptionHandler({BasicException.class})
-    public ResponseError handleBasicExceptions(BasicException basicException){
+    public ResponseEntity<ResponseError> handleBasicExceptions(BasicException basicException){
         LOG.error(basicException.getMessage());
         ResponseError responseError = new ResponseError();
         responseError.setMessage(basicException.getMessage());
-        return responseError;
+        return new ResponseEntity<>(responseError, HttpStatus.NOT_FOUND);
     }
 }
