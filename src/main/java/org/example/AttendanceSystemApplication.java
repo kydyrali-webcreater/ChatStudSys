b91be98b54231b9c06fb11cae3761f9b1a8915c9
imangali <i.kaliakhmetov@strongte.am>
2023-05-08 17:46:09 +0600

    trash

--- src/main/java/org/example/AttendanceSystemApplication.java
+++ src/main/java/org/example/AttendanceSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 
+
 @SpringBootApplication
 public class AttendanceSystemApplication {
     public static void main(String[] args) {
